b401b2c4c536c8d16f3f8044e487d727cb42f553
shabbir3g <mostafizshabbir@gmail.com>
2021-10-31 22:37:48 +0600

    added update delete

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 require('dotenv').config()
+const ObjectId = require('mongodb').ObjectId;
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -24,6 +25,7 @@ async function run(){
         await client.connect();
         const database = client.db("zaraResort");
         const servicesCollection = database.collection('services');
+        const bookingCollection = database.collection('booking');
 
         // GET Services
         app.get('/services', async (req, res) => {
@@ -35,9 +37,65 @@ async function run(){
         // post Services
         app.post('/services', async(req, res) => {
             const service = req.body;
-           const result = await servicesCollection.insertOne(service);
-           res.json(result);
+            const result = await servicesCollection.insertOne(service);
+            res.json(result);
         });
+
+         // post booking
+            app.post('/booking', async(req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.json(result);
+        });
+
+          //GET SINGLE service 
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.json(service)
+        });
+        //GET all booking by email 
+        app.get('/my-orders/:email', async (req, res) => {
+            const email = req.params.email;
+            const service = await bookingCollection.find({email: email}).toArray();
+            res.json(service)
+        });
+
+        //GET all booking
+        app.get('/ManageAllOrders', async (req, res) => {
+            const cursor = bookingCollection.find({});
+            const booking = await cursor.toArray();
+            res.send(booking)
+        });
+        // Delete booking
+        app.delete('/booking/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
+            console.log('deleting users with id', result);
+            res.json(result)
+          });
+        
+
+        // update status
+        app.put('/udpate/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)}
+            const options = { upsert: true };
+            const updatedDoc = {
+              $set: {
+                status: "Approved"
+              },
+           };
+            const result = await bookingCollection.updateOne(filter,updatedDoc, options );
+
+            res.json(result);
+        })
+
+        
+
 
 
     }
